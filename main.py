4efa0b8754b6747f426c29e57cbfcac60e4fba9f
Dayvison Dunga <dayvisond09@gmail.com>
2021-06-07 07:12:40 -0500

    finalizado arquivo main.py com sucesso.

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from questao_01 import q1
+from questao_02 import q2
 from questao_03 import q3
 
-aqui = q3.valorProporcao()
+def menu():
+    print('\nEscolha a baixo qual questão você deseja!\n'
+          '|--------------|\n'
+          '| Questão 1 - 1|\n'
+          '| Questão 2 - 2|\n'
+          '| Questão 3 - 3|\n'
+          '|--------------|\n'
+          '|Encerrar  -  0|')
 
-print(aqui)
+print('Desafio Trainee Bemol - Etapa Python')
+menu()
+
+while True:
+    opcao = input('Qual sua opção? ')
+    print('')
+    if opcao == '1':
+        q1.media()
+        print('\nProxima questão')
+        menu()
+    elif opcao == '2':
+        q2.ranking()
+        print('\nProxima questão')
+        menu()
+    elif opcao == '3':
+        q3.valorProporcao()
+        print('\nProxima questão')
+        menu()
+    elif opcao == '0':
+        print('Fim das questões. Obrigado!')
+        break
+    else:
+        print('Opção invalida')
+        menu()
